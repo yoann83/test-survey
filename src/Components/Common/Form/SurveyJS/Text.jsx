ea678a83f4f3f11ba92a06372c86f1609f1fd1bf
Martinez yoann <41195157+yoann83@users.noreply.github.com>
2022-07-28 13:55:27 +0200

    update files name

--- src/Components/Common/Form/SurveyJS/Text.jsx
+++ src/Components/Common/Form/SurveyJS/Text.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ReactQuestionFactory } from "survey-react";
 import PropTypes from "prop-types";
-import TextEgerie from "../../../TextEgerie";
+import TextEgerie from "../../../TextField";
 
 /* style Overload */
 import "../../scss/sassForm/_Text.scss";
